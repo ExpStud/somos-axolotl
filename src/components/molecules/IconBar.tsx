32b136a7469d6135337a284be7d54eb16b3ec9c4
Darth Degen <darthdegen@gmail.com>
2024-04-17 10:11:59 -0700

    feat: added footer

--- src/components/molecules/IconBar.tsx
+++ src/components/molecules/IconBar.tsx
@@ -1,17 +1,62 @@
-import { motion } from "framer-motion";
 import { FC, HTMLAttributes } from "react";
 import Image from "next/image";
-import { TwitterIcon, DiscordIcon, ExpIcon } from "@components";
+import { useWindowSize } from "src/hooks";
 
-interface Props extends HTMLAttributes<HTMLDivElement> {
-  showExchange?: boolean;
-}
+interface Props extends HTMLAttributes<HTMLDivElement> {}
 const IconBar: FC<Props> = (props: Props) => {
-  const { showExchange = true, className } = props;
+  const { className } = props;
+
+  const [winWidth, winHeight] = useWindowSize();
+  const size = winWidth < 1024 ? 47 : 40;
   return (
-    <div className={`flex items-center justify-center gap-4 ${className}`}>
-      <DiscordIcon url={""} />
-      <TwitterIcon url={""} />
+    <div className={`grid grid-cols-2 gap-3 ${className}`}>
+      <a
+        href={"/"}
+        rel="noreferrer"
+        target="_blank"
+        className="hover-opacity-2"
+      >
+        <Image src="/images/icons/wk.svg" width={size} height={size} alt="Wk" />
+      </a>
+      <a
+        href={"/"}
+        rel="noreferrer"
+        target="_blank"
+        className="hover-opacity-2"
+      >
+        <Image
+          src="/images/icons/facebook.svg"
+          width={size}
+          height={size}
+          alt="Facebook"
+        />
+      </a>
+      <a
+        href={"/"}
+        rel="noreferrer"
+        target="_blank"
+        className="hover-opacity-2"
+      >
+        <Image
+          src="/images/icons/telegram.svg"
+          width={size}
+          height={size}
+          alt="Telegram"
+        />
+      </a>
+      <a
+        href={"/"}
+        rel="noreferrer"
+        target="_blank"
+        className="hover-opacity-2"
+      >
+        <Image
+          src="/images/icons/instagram.svg"
+          width={size}
+          height={size}
+          alt="Instagram"
+        />
+      </a>
     </div>
   );
 };
--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,7 +1,74 @@
 import { FC } from "react";
+import Image from "next/image";
+import { IconBar } from "@components";
+import { useWindowSize } from "src/hooks";
 
 const Footer: FC = () => {
-  return <footer className="" key="footer"></footer>;
+  const [winWidth, winHeight] = useWindowSize();
+
+  return (
+    <footer
+      className="bg-somos-orange flex flex-col md:flex-row justify-evenly md:justify-between md:items-center px-8 pb-2 md:px-10 md:pt-10  md:pb-7 h-[400px] md:h-[220px] lg:h-[200px]"
+      key="footer"
+    >
+      <div className="flex md:flex-col w-full md:w-auto h-min md:h-full justify-between md:justify items-center md:items-start  ">
+        <Image
+          src="/images/logos/lg-brown.svg"
+          alt="Somos"
+          width={winWidth < 768 ? 93 : 65}
+          height={winWidth < 768 ? 131 : 92}
+        />
+        <p className="text-xs hidden md:flex xl:whitespace-nowrap">
+          © {new Date().getFullYear()} — Copyright{" "}
+          <br className="hidden md:block lg:hidden" /> All Rights reserved
+        </p>
+
+        <IconBar className="md:hidden h-min" />
+      </div>
+      <div className="flex justify-between md:justify-end md:items-end gap-8 md:gap-10 md:pb-2">
+        <div className="flex flex-col max-w-[177px]">
+          <h3 className="text-somos-brown-dark tex-sm md:text-base font-poppins-regular pb-3">
+            Contact Us
+          </h3>
+          <a
+            rel="noreferrer"
+            target="_blank"
+            href="tel:+19998887766"
+            className="text-xs"
+          >
+            +1 (999) 888-77-66
+          </a>
+          <a
+            rel="noreferrer"
+            target="_blank"
+            href="mailto:hello@logoipsum.com"
+            className="text-xs"
+          >
+            hello@logoipsum.com
+          </a>
+        </div>
+        <div className="flex flex-col max-w-[177px]">
+          <h3 className="text-somos-brown-dark text-sm md:text-base font-poppins-regular pb-3">
+            Location
+          </h3>
+          <a
+            rel="noreferrer"
+            target="_blank"
+            href="https://www.google.com/maps/search/?api=1&query=483920,Mexico+Myasnitskaya+22/2/5,Office+4"
+            className="line-clamp-2 text-xs"
+          >
+            483920, Mexico <br />
+            Myasnitskaya 22/2/5, Office 4
+          </a>
+        </div>
+        <IconBar className="hidden md:grid w-[100px]" />
+      </div>
+
+      <p className="md:hidden whitespace-nowrap self-center -mb-10 text-xs">
+        © {new Date().getFullYear()} — Copyright All Rights reserved
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
