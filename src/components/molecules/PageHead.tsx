7b480110ca2e7abb4643e6ee11d951409147b03d
Darth Degen <darthdegen@gmail.com>
2024-05-16 06:11:16 -0700

    fix: updated meta url

--- src/components/molecules/PageHead.tsx
+++ src/components/molecules/PageHead.tsx
@@ -20,14 +20,14 @@ const PageHead: FC<Props> = (props: Props) => {
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={`/meta.jpg`} />
+      <meta name="twitter:image" content={`${url}/meta.jpg`} />
       <meta property="twitter:url" content={url} />
       {/* <!-- Open Graph / Facebook --> */}
       <meta property="og:type" content="website" />
       <meta property="og:url" content={url} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={`/meta.jpg`} />
+      <meta property="og:image" content={`${url}/meta.jpg`} />
     </Head>
   );
 };
